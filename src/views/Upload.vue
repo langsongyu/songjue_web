ea3db871c6629ebfc165cd8fd8d24b06ed301640
langsongyu <songyu_lang@163.com>
2021-07-30 11:19:34 +0800

    文件上传功能

---
+++ src/views/Upload.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="test_page">
+    <el-button @click="getTest" size="small" type="primary">GET请求</el-button>
+    <el-button @click="postTest" size="small" type="primary">POST请求</el-button>
+  </div>
+</template>
+
+<script>
+import api from "../net/api";
+export default {
+  name: "upload",
+  setup() {
+    async function getTest() {
+      const params = {};
+      api.getTest(params);
+    }
+
+    async function postTest() {
+      const params = {};
+      api.postTest(params);
+    }
+
+    return {
+      getTest,
+      postTest,
+    };
+  },
+};
+</script>
+
+<style>
+.test_page {
+  padding: 40px;
+}
+
+.el-button {
+  margin-bottom: 15px;
+}
+</style>
